6bfc5af8544e48d915aa79fd1437f49409fa94cf
Franck Tchowa <francktchowa@gmail.com>
2026-09-25 19:08:40 +0100

    chore: update deploy tools.

--- lib/blog/release.ex
+++ lib/blog/release.ex
@@ -23,6 +23,7 @@ defmodule Blog.Release do
   end
 
   defp load_app do
+    # Many platforms require SSL when connecting to the database
     Application.ensure_all_started(:ssl)
     Application.ensure_loaded(@app)
   end
